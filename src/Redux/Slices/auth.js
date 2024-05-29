3049b5042b440fadcf9c61ad2bbb5b038e080d2c
Artem <artem15stepanenko@outlook.com>
2024-05-29 23:31:48 +0300

    registerChanges

--- src/Redux/Slices/auth.js
+++ src/Redux/Slices/auth.js
@@ -39,7 +39,7 @@ export const registerUserData = createAsyncThunk(
 
       return data;
     } catch (error) {
-      console.log(error.message);
+      rejectWithValue(error.message);
     }
   }
 );
