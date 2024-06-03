e8649ffaa7afa5420a7fd76259c4ba5a0e438d3f
Artem <artem15stepanenko@outlook.com>
2024-06-03 10:57:44 +0300

    text

--- src/Components/Authorization/BaseText.jsx
+++ src/Components/Authorization/BaseText.jsx
@@ -4,8 +4,8 @@ export default function BaseText() {
   return (
     <section className={styles.main_page_content_box}>
       <p>
-        WMS в браузере Что нужно - в одной системе: поступления, хранения,
-        перемещения, отгрузки.
+        WMS в браузере <br /> Что нужно - в одной системе: поступления,
+        хранения, перемещения, отгрузки.
       </p>
     </section>
   );
